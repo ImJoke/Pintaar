80f61327e1a5597209072217ebfc2ad1e13fe521
ImJoke <55929550+ImJoke@users.noreply.github.com>
2021-02-13 11:21:11 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -2,17 +2,30 @@ import os as o, webbrowser as w, platform as p # imports
 link = ("easteregg","https://youtu.be/Drgg4VpQpFI", "https://youtu.be/HvMGPvduqGg", "https://youtu.be/Rh5MYx02iNM", "https://youtu.be/rFnXvhX9GLU", "https://youtu.be/nqA3YVx72Ic", "https://youtu.be/Qi3iiu_bRR8", "https://youtu.be/CotoizBwjqI", "https://youtu.be/wGo4vf8BFvU", "https://youtu.be/nkgGBBGJeSA", "https://youtu.be/514yU6WcPGk", "https://youtu.be/bBtl_h9lYd0") # links
 
 def mulai():
+    global sistemc
     if p.system() == "Windows" or p.system() == "win64" or p.system() == "win32":
+        sistemc = "cls"
         o.system("cls")
         menu()
+        error()
+        pilih()
+
     elif p.system() == "Linux" or p.system() == "OS X" or p.system() == "linux" or p.system() == "linux2" or p.system() == "darwin" or p.system() == "Darwin":
+        sistemc = "clear"
         o.system("clear")
         menu()
+        error()
+        pilih()
+
     else:
         print("OS Anda tidak di support\n")
         android = input("Apakah Anda menggunakan Android?\n[y/n] = ")
         if android == "y" or android == "Y" :
+            sistemc = "clear"
+            o.system("clear")
             menu()
+            error()
+            pilih()
         
         elif android == "n" or android == "N" :
             print("Bye\n")
@@ -20,6 +33,7 @@ def mulai():
             for i in range (7):
                 t.sleep(1)
                 print('{:2d}'.format(5-i),end="\r")
+            sistemc = ""
             o.system("exit")
 
         else:
@@ -28,45 +42,60 @@ def mulai():
 
 def menu():
     print("\n1. Pengenalan\n2. Game Development\n3. Tips untuk Game Development\n4. Mengenal Game Dadulfall Post Mortem\n5. Instalasi unity & setup project\n6. Membuat player\n7. Player bisa gerak & game manager\n8. Membuat UI\n9. Membuat peluru player\n10. Membuat musuh\n11. Musuh bisa menembak\n12. Download e-certificate")
-    pilih()
+    
+def error():
+    print("\n\n99. Error, link tidak membuka browser")
 
 def pilih():
     global pilih
-    pilih = input("\nPilih = ") # Pemilihan
-    if pilih != "" :
-        pembuka()
+    try :
+        pilih = int(input("\nPilih = ")) # Pemilihan
+        if pilih != "" :
+            pembuka()
 
-    else:
-        print("Masukkan angkanya contoh 1 atau bisa juga 1.")
-        mulai()
+        else:
+            print("Masukkan angkanya contoh 1 atau bisa juga 1.")
+            mulai()
+
+    except ValueError:
+        if sistemc != "" :
+            o.system(sistemc)
+        print("\nAnda hanya boleh memasukkan angka\n\n")
 
 def pembuka():
-    if pilih == "1" or pilih == "1." :
-        w.open(link[1])
-    elif pilih == "2" or pilih == "2." :
-        w.open(link[2])
-    elif pilih == "3" or pilih == "3." :
-        w.open(link[3])
-    elif pilih == "4" or pilih == "4" :
-        w.open(link[4])
-    elif pilih == "5" or pilih == "5." :
-        w.open(link[5])
-    elif pilih == "6" or pilih == "6." :
-        w.open(link[6])
-    elif pilih == "7" or pilih == "7." :
-        w.open(link[7])
-    elif pilih == "8" or pilih == "8." :
-        w.open(link[8])
-    elif pilih == "9" or pilih == "9." :
-        w.open(link[9])
-    elif pilih == "10" or pilih == "10." :
-        w.open(link[10])
-    elif pilih == "11" or pilih == "11." :
-        w.open(link[11])
-    elif pilih == "12" or pilih == "12." :
-        w.open(link[12])
+    if pilih != "" and pilih != int("0") and pilih != int("99"):
+        w.open(link[pilih])
+
+    elif pilih == int("0") :
+        if p.system() == "Windows" or p.system() == "win64" or p.system() == "win32":
+            w.open(link[pilih])
+
+        else:
+            o.system(sistemc)
+            print("\nMaaf angka 0 hanya tersedia untuk Windows (Windows 10)\n\n")
+
+    elif pilih == int("99") :
+        o.system(sistemc)
+        menu()
+
+        pilihError = int(input("\n[Error mode]\nPilih  = "))
+        if pilihError != "" and pilihError != int("0"):
+            print("\nLink video =", link[pilihError], end="\n\n")
+
+        elif pilihError == int("0") :
+            if p.system() == "Windows" or p.system() == "win64" or p.system() == "win32":
+                w.open(link[pilihError])
+
+            else:
+                o.system(sistemc)
+                print("\nMaaf angka 0 hanya tersedia untuk Windows (Windows 10)\n\n")
+
+        else:
+            print("Masukkan angkanya contoh 1 atau bisa juga 1.")
+            mulai()
+
     else:
-        print("Masukkan angkanya contoh 1 atau bisa juga 1.")
+        print("Masukkan angkanya contoh \"1\"")
         mulai()
 
 if __name__ == "__main__" :
